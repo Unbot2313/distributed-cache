b27f93ce1467b907ac683fd511ea01f8231cb241
Unbot2313 <126409052+Unbot2313@users.noreply.github.com>
2025-09-01 11:08:17 -0500

    feature: hasher with xxh3
    interfaz con las funciones principales de hash para inversion de dependencias y funcionalidades

--- pkg/hash/hasher.go
+++ pkg/hash/hasher.go
@@ -1,2 +1,26 @@
 package hash
 
+import "github.com/zeebo/xxh3"
+
+type Hasher interface {
+    Hash(key string) uint32
+    HashWithSeed(key string, seed uint64) uint32
+}
+
+// struct para interface
+type XXH3Hasher struct{}
+
+// Constructor
+func NewXXH3Hasher() *XXH3Hasher {
+    return &XXH3Hasher{}
+}
+
+// Hash key
+func (h *XXH3Hasher) Hash(key string) uint32 {
+    return uint32(xxh3.HashString(key))
+}
+
+// Hash con seed para los nodos virtuales
+func (h *XXH3Hasher) HashWithSeed(key string, seed uint64) uint32 {
+    return uint32(xxh3.HashStringSeed(key, seed))
+}
