a21bc0d5c154e638dabf77e48755e4385083bfea
Unbot2313 <126409052+Unbot2313@users.noreply.github.com>
2025-09-02 11:24:11 -0500

    feature: diferenciacion de nodos fisicos y nodos virtuales

--- pkg/ring/node.go
+++ pkg/ring/node.go
@@ -1,19 +1,24 @@
 package ring
 
 import (
+	"fmt"
+
 	"github.com/unbot2313/distributed-cache/pkg/hash"
 )
 
 type Nodes []*Node
 
 type Node struct {
-	Id     int
-	HashId uint32
+    PhysicalId string  // "server-1" 
+    VirtualId  string  // "server-1:0", "server-1:1", etc
+    HashId     uint32  // hash("server-1:0")
 }
 
-func NewNode(Hasher hash.Hasher, id int) *Node{
-	  return &Node{
-		Id:     id,
-		HashId: Hasher.Hash(id),
-	}
+func NewVirtualNode(hasher hash.Hasher, physicalId string, virtualIndex int) *Node {
+    virtualId := fmt.Sprintf("%s:%d", physicalId, virtualIndex)
+    return &Node{
+        PhysicalId: physicalId,
+        VirtualId:  virtualId,
+        HashId:     hasher.Hash(virtualId),
+    }
 }
