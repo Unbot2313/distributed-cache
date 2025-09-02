de52ec4d3338281e59fb42a35f67ec7ce0f6d66b
Unbot2313 <126409052+Unbot2313@users.noreply.github.com>
2025-09-02 11:15:38 -0500

    feature: Sort of nodes and AddNode function

--- pkg/ring/ring.go
+++ pkg/ring/ring.go
@@ -1,6 +1,8 @@
 package ring
 
 import (
+	"sort"
+
 	"github.com/unbot2313/distributed-cache/pkg/hash"
 )
 
@@ -24,8 +26,12 @@ func NewRing(Hasher hash.Hasher, VirtualNodes int) *consistentHash {
 	}
 }
 
-// func (r *Ring) AddNode(id string) {
-// 	node := NewNode(r.Hasher, id)
-// 	r.Nodes = append(r.Nodes, *node)
-// 	sort.Sort(r.Nodes)
-// }
+func (r *consistentHash) AddNode(id int) {
+	// se añade la cantidad de nodos segund los nodos virtuales
+	for i := 0; i < r.virtualNodes; i++ {
+		Node := NewNode(r.Hasher, id)
+		r.Nodes = append(r.Nodes, Node)
+	}
+	sort.Sort(r.Nodes)
+}
+
