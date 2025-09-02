56e82a8b1d28913e7386d45f681e058a8c96442a
Unbot2313 <126409052+Unbot2313@users.noreply.github.com>
2025-09-02 11:07:04 -0500

    feature: Node type and Ring type

--- pkg/ring/node.go
+++ pkg/ring/node.go
@@ -1,4 +1,19 @@
 package ring
 
+import (
+	"github.com/unbot2313/distributed-cache/pkg/hash"
+)
+
+type Nodes []*Node
+
 type Node struct {
+	Id     int
+	HashId uint32
+}
+
+func NewNode(Hasher hash.Hasher, id int) *Node{
+	  return &Node{
+		Id:     id,
+		HashId: Hasher.Hash(id),
+	}
 }
--- pkg/ring/ring.go
+++ pkg/ring/ring.go
@@ -1,6 +1,31 @@
 package ring
 
-type Ring struct {
+import (
+	"github.com/unbot2313/distributed-cache/pkg/hash"
+)
+
+type consistentHash struct {
+	Nodes Nodes
+	Hasher hash.Hasher
+	virtualNodes int // Number of virtual nodes per physical node
 }
 
+type Ring interface {
+	AddNode(Id int)
+	GetNode(Id int) *Node
+	DeleteNode(id int) error
+}
 
+func NewRing(Hasher hash.Hasher, VirtualNodes int) *consistentHash {
+	return &consistentHash{
+		Nodes:      Nodes{},
+		Hasher:    Hasher,
+		virtualNodes:  VirtualNodes,
+	}
+}
+
+// func (r *Ring) AddNode(id string) {
+// 	node := NewNode(r.Hasher, id)
+// 	r.Nodes = append(r.Nodes, *node)
+// 	sort.Sort(r.Nodes)
+// }
