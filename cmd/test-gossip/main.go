db77b8aff5d4849458c2ef96c6d9c6841d2717dd
Tomas Ramirez Galeano <126409052+Unbot2313@users.noreply.github.com>
2026-03-18 08:15:11 -0500

    fix: flags for gossip

--- cmd/test-gossip/main.go
+++ cmd/test-gossip/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"os"
 	"time"
+	"flag"
 	"github.com/unbot2313/distributed-cache/pkg/gossip"
 )
 
 func main() {
-	addrUDPArg := os.Args[2]
+	// Command-line arguments
+	gossipPortArg := flag.String("gossip", "7000", "UDP port to listen on for gossip communication")
+	// httpPortArg := flag.String("http", "3211", "HTTP port to listen on")
+	seedsArg := flag.String("seeds", "", "Comma-separated list of seed nodes in the format <ip>:<port>")
+
+	flag.Parse()
 
 	// gossip communication service
 	gossipService := gossip.NewGossipCommunicationService()
-	gossip := gossip.NewGossip(addrUDPArg, gossipService)
-	go gossip.StartListener(addrUDPArg)
+	gossip := gossip.NewGossip("localhost:" + *gossipPortArg, gossipService)
+	gossip.SetSeedNodes(*seedsArg)
+	go gossip.StartListener(*gossipPortArg)
 	gossip.StartGossiping(5 * time.Second)
 }
